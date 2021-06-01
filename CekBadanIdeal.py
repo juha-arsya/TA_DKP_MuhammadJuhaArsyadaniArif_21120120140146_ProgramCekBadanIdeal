8759b29d141471226ac713385920fa1b534cd3ea
M. Juha Arsyadani Arif <82462900+juhaarsya@users.noreply.github.com>
2021-06-01 17:59:40 +0700

    final

--- CekBadanIdeal.py
+++ CekBadanIdeal.py
@@ -4,23 +4,23 @@ from tkinter import messagebox
 
 def reset_entry():
     nama_tf.delete(0, 'end')
-    height_tf.delete(0, 'end')
-    weight_tf.delete(0, 'end')
+    berat_tf.delete(0, 'end')
+    tinggi_tf.delete(0, 'end')
 
 
-def ucapan_kata():
+def hitung_bmi():
+    kg = int(berat_tf.get())
+    m = int(tinggi_tf.get())/100
+    bmi = kg/(m*m)
+    bmi = round(bmi, 1)
+    bmi_index(bmi)
+
+
+def ucapan():
     n = 0
     for n in range(6):
         print("Terimakasih", (n))
     n += 1
-
-
-def calculate_bmi():
-    kg = int(weight_tf.get())
-    m = int(height_tf.get())/100
-    bmi = kg/(m*m)
-    bmi = round(bmi, 1)
-    bmi_index(bmi)
 
 
 def bmi_index(bmi):
@@ -53,48 +53,49 @@ gen_lb.grid(row=1, column=1)
 frame2 = Frame(frame)
 frame2.grid(row=1, column=2, pady=5)
 
-male_rb = Radiobutton(frame2, text='Pria', variable=var, value=1, bg='#a1eafb')
-male_rb.pack(side=LEFT)
+pria_rb = Radiobutton(frame2, text='Pria', variable=var, value=1, bg='#a1eafb')
+pria_rb.pack(side=LEFT)
 
-female_rb = Radiobutton(frame2, text='Wanita',
+wanita_rb = Radiobutton(frame2, text='Wanita',
                         variable=var, value=2, bg='#a1eafb')
-female_rb.pack(side=RIGHT)
+wanita_rb.pack(side=RIGHT)
 
-# Name
+# Nama
 nama_lb = Label(frame, text="Nama : ", bg='#a1eafb')
 nama_lb.grid(row=2, column=1)
 nama_tf = Entry(frame, bg='#fdfdfd')
 nama_tf.grid(row=2, column=2, pady=5)
 
-# Height & Weight
-weight_lb = Label(frame, text="Berat Badan (kg) : ", bg='#a1eafb')
-weight_lb.grid(row=3, column=1)
-weight_tf = Entry(frame, bg='#fdfdfd')
-weight_tf.grid(row=3, column=2, pady=5)
+# Berat
+berat_lb = Label(frame, text="Berat Badan (kg) : ", bg='#a1eafb')
+berat_lb.grid(row=3, column=1)
+berat_tf = Entry(frame, bg='#fdfdfd')
+berat_tf.grid(row=3, column=2, pady=5)
 
-height_lb = Label(frame, text="Tinggi Badan (cm) : ", bg='#a1eafb')
-height_lb.grid(row=4, column=1)
-height_tf = Entry(frame, bg='#fdfdfd')
-height_tf.grid(row=4, column=2, pady=5)
+# Tinggi
+tinggi_lb = Label(frame, text="Tinggi Badan (cm) : ", bg='#a1eafb')
+tinggi_lb.grid(row=4, column=1)
+tinggi_tf = Entry(frame, bg='#fdfdfd')
+tinggi_tf.grid(row=4, column=2, pady=5)
 
 # Button Hitung, Reset, Exit, Terimakasih
 frame3 = Frame(frame)
 frame3.grid(row=5, columnspan=3, pady=10)
 
-cal_btn = Button(frame3, text='Hitung', command=calculate_bmi,
-                 bg='#4ecca3')
-cal_btn.pack(side=LEFT)
+hitung_btn = Button(frame3, text='Hitung', command=hitung_bmi,
+                    bg='#4ecca3')
+hitung_btn.pack(side=RIGHT)
+
+keluar_btn = Button(frame3, text='Keluar',
+                    command=lambda: app.destroy(), bg='#cf0000', fg='white')
+keluar_btn.pack(side=LEFT)
 
 reset_btn = Button(frame3, text='Reset', command=reset_entry,
                    bg='#3d6cb9', fg='white')
 reset_btn.pack(side=LEFT)
 
-exit_btn = Button(frame3, text='Exit',
-                  command=lambda: app.destroy(), bg='#3d6cb9', fg='white')
-exit_btn.pack(side=RIGHT)
-
-ucapan_btn = Button(frame, text='Terimakasih',
-                    command=ucapan_kata, bg='#3d6cb9', fg='white')
-ucapan_btn.grid(row=6, columnspan=3, pady=10)
+trim_btn = Button(frame, text='Terimakasih',
+                  command=ucapan, bg='#3d6cb9', fg='white')
+trim_btn.grid(row=6, columnspan=3, pady=10)
 
 app.mainloop()
